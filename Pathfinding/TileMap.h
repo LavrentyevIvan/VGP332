f1b8a23e5568526b991db284bb655f8cb635a1b7
ILavrentyev6422 <ilavrentyev6422@gmail.com>
2024-01-26 19:05:51 -0800

    Ai pathing algorithms

--- Pathfinding/TileMap.h
+++ Pathfinding/TileMap.h
@@ -1,6 +1,18 @@
 #pragma once
 
 #include <XEngine.h>
+#include <AI.h>
+#include "GridBasedGraph.h"
+
+
+using Path = std::vector<X::Math::Vector2>;
+
+struct Tile
+{
+	X::TextureId textureId;
+	bool isWalkable = false;
+};
+
 
 class TileMap
 {
@@ -10,12 +22,23 @@ public:
 
 	void Render() const;
 
+	bool IsBlocked(int x, int y) const;
+	int getColumns() const { return mColumns; }
+	int getRows() const { return mRows; }
+
+	X::Math::Vector2 GetPixelPosition(int x, int y)const;
+
+	Path FindPathBFS(int startX, int startY, int endX, int endY);
+	Path FindPathDFS(int startX, int startY, int endX, int endY);
+	Path FindPathDijkstra(int startX, int startY, int endX, int endY);
+
 private:
+
+	AI::GridBasedGraph mGraph;
 	std::vector<int> mMap;
-	std::vector<X::TextureId> mTiles; // I forgot to add this in class
+	std::vector<Tile> mTiles; 
 	int mColumns = 0;
 	int mRows = 0;
 	int mTileWidth = 0;
-	int mTileHeight = 0;
-
+	int mTileheight = 0;
 };
--- Pathfinding/WinMain.cpp
+++ Pathfinding/WinMain.cpp
@@ -5,6 +5,15 @@
 TileMap tileMap;
 X::TextureId textureId;
 X::Math::Vector2 position;
+Path path;
+
+
+int startX = 5;
+int startY = 9;
+
+int endX = 15;
+int endY = 12;
+
 
 //--------------------------------------------------
 
@@ -29,11 +38,33 @@ bool GameLoop(float deltaTime)
 	else if (X::IsKeyDown(X::Keys::UP))
 		position.y -= moveSpeed * deltaTime;
 
-	ImGui::Begin("Pathfinding", nullptr, ImGuiWindowFlags_AlwaysAutoResize); {
-		int test = 0;
-		ImGui::DragInt("TestValue", &test);
-		ImGui::Button("Press");
+	
+	ImGui::Begin("PathFinding", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+	{
+		const int columns = tileMap.getColumns();
+		const int rows = tileMap.getRows();
+
+		ImGui::DragInt("Start X", &startX, 1, 0 , columns -1);
+		ImGui::DragInt("Start Y", &startY, 1, 0, rows - 1);
+
+		ImGui::DragInt("End X", &endX, 1, 0, columns - 1);
+		ImGui::DragInt("End Y", &endY, 1, 0, rows - 1);
+		
+		if (ImGui::Button("RunBFS"))
+		{
+			path = tileMap.FindPathBFS(startX, startY, endX, endY);
+		}
+		if (ImGui::Button("RunDFS"))
+		{
+			path = tileMap.FindPathDFS(startX, startY, endX, endY);
+		}
+		if (ImGui::Button("RunDijkstra"))
+		{
+			path = tileMap.FindPathDijkstra(startX, startY, endX, endY);
+		}
+
 	}
+
 	ImGui::End();
 
 	tileMap.Render();
@@ -41,7 +72,18 @@ bool GameLoop(float deltaTime)
 	X::DrawSprite(textureId, position, X::Pivot::TopLeft, X::Flip::Horizontal);
 	X::DrawScreenDiamond(position, 5.0f, X::Colors::Cyan);
 
+
+
+	for (int i = 1; i < path.size(); ++i)
+	{
+		X::DrawScreenLine(path[i - 1], path[i], X::Colors::Red);
+	}
+
+	X::DrawScreenCircle(tileMap.GetPixelPosition(startX,startY), 10.0f, X::Colors::Pink);
+	X::DrawScreenCircle(tileMap.GetPixelPosition(endX, endY), 10.0f, X::Colors::Yellow);
+
 	const bool quit = X::IsKeyPressed(X::Keys::ESCAPE);
+
 	return quit;
 }
 
